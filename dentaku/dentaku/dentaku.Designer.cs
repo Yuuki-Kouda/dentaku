aa917ea0ce130f2511622741d923f7cce3cd409e
koda-y <koda-y@evoltech.co.jp>
2019-10-04 11:11:57 +0900

    Version-0

---
+++ dentaku/dentaku/dentaku.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace dentaku
+{
+    partial class dentaku
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button0 = new System.Windows.Forms.Button();
+			this.cmddiv = new System.Windows.Forms.Button();
+			this.cmdmpc = new System.Windows.Forms.Button();
+			this.cmdminus = new System.Windows.Forms.Button();
+			this.cmdplus = new System.Windows.Forms.Button();
+			this.cmdeq = new System.Windows.Forms.Button();
+			this.cmdclear = new System.Windows.Forms.Button();
+			this.cmddp = new System.Windows.Forms.Button();
+			this.hugou = new System.Windows.Forms.TextBox();
+			this.enzanshi = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox1.Location = new System.Drawing.Point(12, 21);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(194, 34);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button1.Location = new System.Drawing.Point(12, 70);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(44, 44);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button2.Location = new System.Drawing.Point(62, 70);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(44, 44);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button3.Location = new System.Drawing.Point(112, 70);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(44, 44);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button4.Location = new System.Drawing.Point(12, 120);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(44, 44);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button5.Location = new System.Drawing.Point(62, 120);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(44, 44);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button6
+			// 
+			this.button6.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button6.Location = new System.Drawing.Point(112, 120);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(44, 44);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button7
+			// 
+			this.button7.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button7.Location = new System.Drawing.Point(12, 170);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(44, 44);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button8
+			// 
+			this.button8.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button8.Location = new System.Drawing.Point(62, 170);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(44, 44);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button9
+			// 
+			this.button9.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button9.Location = new System.Drawing.Point(112, 170);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(44, 44);
+			this.button9.TabIndex = 9;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// button0
+			// 
+			this.button0.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button0.Location = new System.Drawing.Point(12, 220);
+			this.button0.Name = "button0";
+			this.button0.Size = new System.Drawing.Size(44, 44);
+			this.button0.TabIndex = 10;
+			this.button0.Text = "0";
+			this.button0.UseVisualStyleBackColor = true;
+			this.button0.Click += new System.EventHandler(this.buttonnum);
+			// 
+			// cmddiv
+			// 
+			this.cmddiv.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.cmddiv.Location = new System.Drawing.Point(162, 70);
+			this.cmddiv.Name = "cmddiv";
+			this.cmddiv.Size = new System.Drawing.Size(44, 44);
+			this.cmddiv.TabIndex = 12;
+			this.cmddiv.Text = "/";
+			this.cmddiv.UseVisualStyleBackColor = true;
+			this.cmddiv.Click += new System.EventHandler(this.cmdplus_Click);
+			// 
+			// cmdmpc
+			// 
+			this.cmdmpc.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.cmdmpc.Location = new System.Drawing.Point(162, 120);
+			this.cmdmpc.Name = "cmdmpc";
+			this.cmdmpc.Size = new System.Drawing.Size(44, 44);
+			this.cmdmpc.TabIndex = 13;
+			this.cmdmpc.Text = "*";
+			this.cmdmpc.UseVisualStyleBackColor = true;
+			this.cmdmpc.Click += new System.EventHandler(this.cmdplus_Click);
+			// 
+			// cmdminus
+			// 
+			this.cmdminus.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.cmdminus.Location = new System.Drawing.Point(162, 170);
+			this.cmdminus.Name = "cmdminus";
+			this.cmdminus.Size = new System.Drawing.Size(44, 44);
+			this.cmdminus.TabIndex = 14;
+			this.cmdminus.Text = "-";
+			this.cmdminus.UseVisualStyleBackColor = true;
+			this.cmdminus.Click += new System.EventHandler(this.cmdplus_Click);
+			// 
+			// cmdplus
+			// 
+			this.cmdplus.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.cmdplus.Location = new System.Drawing.Point(162, 220);
+			this.cmdplus.Name = "cmdplus";
+			this.cmdplus.Size = new System.Drawing.Size(44, 44);
+			this.cmdplus.TabIndex = 15;
+			this.cmdplus.Text = "+";
+			this.cmdplus.UseVisualStyleBackColor = true;
+			this.cmdplus.Click += new System.EventHandler(this.cmdplus_Click);
+			// 
+			// cmdeq
+			// 
+			this.cmdeq.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.cmdeq.Location = new System.Drawing.Point(112, 220);
+			this.cmdeq.Name = "cmdeq";
+			this.cmdeq.Size = new System.Drawing.Size(44, 44);
+			this.cmdeq.TabIndex = 16;
+			this.cmdeq.Text = "=";
+			this.cmdeq.UseVisualStyleBackColor = true;
+			this.cmdeq.Click += new System.EventHandler(this.cmdeq_Click);
+			// 
+			// cmdclear
+			// 
+			this.cmdclear.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.cmdclear.Location = new System.Drawing.Point(162, 270);
+			this.cmdclear.Name = "cmdclear";
+			this.cmdclear.Size = new System.Drawing.Size(44, 44);
+			this.cmdclear.TabIndex = 17;
+			this.cmdclear.Text = "c";
+			this.cmdclear.UseVisualStyleBackColor = true;
+			this.cmdclear.Click += new System.EventHandler(this.cmdclear_Click);
+			// 
+			// cmddp
+			// 
+			this.cmddp.Location = new System.Drawing.Point(62, 223);
+			this.cmddp.Name = "cmddp";
+			this.cmddp.Size = new System.Drawing.Size(44, 41);
+			this.cmddp.TabIndex = 18;
+			this.cmddp.Text = ".";
+			this.cmddp.UseVisualStyleBackColor = true;
+			this.cmddp.Click += new System.EventHandler(this.cmddp_Click);
+			// 
+			// hugou
+			// 
+			this.hugou.Location = new System.Drawing.Point(2, 12);
+			this.hugou.Name = "hugou";
+			this.hugou.Size = new System.Drawing.Size(17, 19);
+			this.hugou.TabIndex = 19;
+			// 
+			// enzanshi
+			// 
+			this.enzanshi.Location = new System.Drawing.Point(198, 12);
+			this.enzanshi.Name = "enzanshi";
+			this.enzanshi.Size = new System.Drawing.Size(17, 19);
+			this.enzanshi.TabIndex = 20;
+			// 
+			// dentaku
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(218, 323);
+			this.Controls.Add(this.enzanshi);
+			this.Controls.Add(this.hugou);
+			this.Controls.Add(this.cmddp);
+			this.Controls.Add(this.cmdclear);
+			this.Controls.Add(this.cmdeq);
+			this.Controls.Add(this.cmdplus);
+			this.Controls.Add(this.cmdminus);
+			this.Controls.Add(this.cmdmpc);
+			this.Controls.Add(this.cmddiv);
+			this.Controls.Add(this.button0);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "dentaku";
+			this.Text = "電卓";
+			this.Click += new System.EventHandler(this.cmdplus_Click);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button cmddiv;
+        private System.Windows.Forms.Button cmdmpc;
+        private System.Windows.Forms.Button cmdminus;
+        private System.Windows.Forms.Button cmdplus;
+        private System.Windows.Forms.Button cmdeq;
+        private System.Windows.Forms.Button cmdclear;
+        private System.Windows.Forms.Button cmddp;
+		private System.Windows.Forms.TextBox hugou;
+		private System.Windows.Forms.TextBox enzanshi;
+	}
+}
+
---
+++ dentaku/dentaku/dentaku.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dentaku
+{
+	public partial class dentaku : Form
+	{
+		public dentaku()
+		{
+			InitializeComponent();
+		}
+
+		string Input_str = ""; //入力数字
+		string  sign = "";     //符号
+		double result = 0;     //計算結果
+		string ope = null;     //演算子
+		bool opeflg = false;   //演算子判定フラグ
+		bool dpflg = false;    //小数点フラグ
+		bool eqflg = false;    //イコールフラグ
+
+		///イベント///
+		//数字入力
+		private void buttonnum(object sender, EventArgs e)
+		{
+
+		}
+
+		//小数点入力
+		private void cmddp_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		//演算子入力
+		private void cmdplus_Click(object sender, EventArgs e)
+		{
+			Input_str = ((Button)sender).Text;
+			string text = textBox1.Text;
+
+			if (opeflg)
+			{
+				//演算処理
+
+				ope = Input_str;
+			}
+
+			else
+			{
+				enzanshi.Text = Input_str;
+			}
+		}
+
+		//イコール入力
+		private void cmdeq_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		//クリア入力
+		private void cmdclear_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+
+	///メソッド///
+	//演算処理
+	private double calmethod(string ope, double inputNum, double result)
+	{
+		switch (ope)
+		{
+			case "+":
+				result += inputNum;
+
+				break;
+
+			case "-":
+				result -= inputNum;
+
+				break;
+
+			case "*":
+				result *= inputNum;
+
+				break;
+
+			case "/":
+				result /= inputNum;
+
+				break;
+
+		}
+
+		return result;
+
+	}
+
+	//桁数チェック
+	private bool nodcheck(string strNum)
+	{
+		double num = double.Parse(strNum);
+
+		if (num.) ;
+	}
+
+
+}
+
